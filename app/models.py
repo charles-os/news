d163a03a456f80154d2bf9aa1fb1ef341bbe2dc1
Munyiwamwangi <josseymunyi@gmail.com>
2019-07-28 08:05:48 +0300

    add articles class

--- app/models.py
+++ app/models.py
@@ -14,28 +14,13 @@ class News:
 		self.category = category
 
 
-class Review:
-    all_reviews = []
-    def  __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
+class Article:
+	def __init__(self,source,urlToImage,author,title,description,publishedAt,url):
 
-    def save_review(self):
-
-        Review.all_reviews.append(self)
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls, id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-        return response
+		self.source = source
+		self.author = author
+		self.title = title
+		self.description = description
+		self.publishedAt = publishedAt
+		self.urlToImage = urlToImage
+		self.url = url
